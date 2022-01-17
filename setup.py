6f5a45d8c2403c1f073c1f69cbf65c535a2b5e1c
viniciusLambert <huevinne@gmail.com>
2022-01-17 06:20:23 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='distributions',
+setup(name='vnl_distributions',
       version='0.1',
-      description='Gaussian distributions',
+      description='Gaussian and Binomials distributions',
       packages=['distributions'],
       zip_safe=False)
