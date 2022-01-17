c24dab06d8346527da9b7a59c856081dca8cd9ac
viniciusLambert <huevinne@gmail.com>
2022-01-17 06:42:00 -0300

    last adjusts to upload to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
 setup(name='vnl_distributions',
-      version='0.1',
+      version='0.2',
       description='Gaussian and Binomials distributions',
-      packages=['distributions'],
+      packages=['vnl_distributions'],
+      author= 'Vinicius Nascimento',
+      author_email= 'huevinne@gmail.com',
       zip_safe=False)
---
+++ vnl_distributions/README.md
@@ -0,0 +1,11 @@
+# distributions
+A python package for Gaussian and Binomials distribution.
+
+
+
+## Authors
+
+The code was made by Vinicius Nascimento, with help of Udacity Data science Nano Degree.
+
+
+
